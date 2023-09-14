d8da79d2e88c5c8f8b109b302dd53b4ecf3975fc
Matt Peck <mpeck5@uwo.ca>
2023-09-14 16:16:52 +0200

    UI is no Longer Fun Scene
    Well, it don't exactly look like I want, but we can track the top 5 scores.

--- Assets/Scripts/PollforScores.cs
+++ Assets/Scripts/PollforScores.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Utilities;
 
 public class PollforScores : MonoBehaviour
 {
     List<ScoreInstance> toBeDisplayed;
     public GameObject ScoresGrid;
-    public GameObject scorePrefab;
+    //public GameObject scorePrefab;
+    //GridObjectCollection theGrid;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //theGrid = ScoresGrid.GetComponent<GridObjectCollection>();
     }
 
     // Update is called once per frame
@@ -21,12 +23,17 @@ public class PollforScores : MonoBehaviour
 
     void OnEnable()
     {
+        //theGrid = ScoresGrid.GetComponent<GridObjectCollection>();
         toBeDisplayed = XMLManager.instance.LoadScores();
-        for (int i = 0; i < toBeDisplayed.Count; i++)
+        for (int i = 0; i < 5; i++)
         {
-            GameObject temp = Instantiate(scorePrefab);
+            GameObject temp = ScoresGrid.gameObject.transform.GetChild(i).gameObject;
             //Change the text fields
+            PopulateScore theScript = temp.GetComponent<PopulateScore>();
+            theScript.Populate(toBeDisplayed[i], i+1);
             //Attach as child
+            temp.transform.parent = ScoresGrid.transform;
         }
+        //theGrid.UpdateCollection();
     }
 }
